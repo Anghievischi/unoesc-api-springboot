1e81193047af1fb5034d4755d30c61f198b89f98
Diogenes <diogenes.angk@hotmail.com>
2023-04-18 03:22:00 -0300

    Start (project api)

---
+++ src/main/java/unoesc/api/unoescapispringboot/UnoescApiSpringbootApplication.java
@@ -0,0 +1,13 @@
+package unoesc.api.unoescapispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnoescApiSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnoescApiSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/unoesc/api/unoescapispringboot/UnoescApiSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package unoesc.api.unoescapispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnoescApiSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
