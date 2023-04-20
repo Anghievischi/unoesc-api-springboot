acf9a37e9b91e8a68dfd8d85e47c089a1afd3277
Diogenes <diogenes.angk@hotmail.com>
2023-04-20 14:44:13 -0300

    Fiz introdução do DTO ao codigo

---
+++ src/main/java/unoesc/api/unoescapispringboot/entities/QuoteDTO.java
@@ -0,0 +1,13 @@
+package unoesc.api.unoescapispringboot.entities;
+
+public class QuoteDTO {
+    private String symbol;
+    private String shortName;
+    private Double regularMarketPrice;
+    private Long marketCap;
+    private Long regularMarketVolume;
+    private String currency;
+    private Long regularMarketTime;
+
+    // construtor, getters e setters
+}
